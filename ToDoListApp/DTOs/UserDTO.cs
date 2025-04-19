e24cc21d4808ae339b17034eaae3d5365ff8fd2c
Varnan <varnanpalan@gmail.com>
2025-04-20 01:48:41 +0200

    relation mellem todotask og user

---
+++ ToDoListApp/DTOs/UserDTO.cs
@@ -0,0 +1,10 @@
+namespace ToDoListApp.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+    }
+}
