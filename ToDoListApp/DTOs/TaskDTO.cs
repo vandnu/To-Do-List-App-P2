1c052edef991d983220f9f0464ba39b07276efe3
Varnan <varnanpalan@gmail.com>
2025-05-13 01:21:32 +0200

    sletter ældre todolidtapp mappe som før var api eller lowkey ja altså ændret jo navn så tror det er derfor den er der

--- ToDoListApp/DTOs/TaskDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace ToDoListApp.DTOs
-{
-    public class TaskDto
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public bool IsCompleted { get; set; } // De vigtigste data?
-    }
-}
