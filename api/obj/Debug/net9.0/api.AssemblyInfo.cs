4c5619ba96683ddb638bf1b54f34b04f0afe8935
Varnan <varnanpalan@gmail.com>
2025-05-27 22:29:56 +0200

    gg Tailwind virker vist ikke

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef18e08f4db221bdea88d3aa04af803fef4e1e43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae58ba1f3f8eb0c8c8a0c626e6a10db095cdd7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
